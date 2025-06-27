ba383f83b7a86e1b6660fc1ce072f0b755045c40
andre-flint <andre.flint@thenorthwest.com>
2025-06-27 08:56:51 -0400

    Delete config.php

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-return [
-    'clientId' => '0d04b387a72628cb3408e18a7dc1b4644eaba3b7008daecd7ffe7533ac8acbb2',
-    'clientSecret' => 'f0ebe7656a671cf92078e90faaffce77efda6cb77bd691d303a196d82406b8ee',
-    'redirectUri' => 'http://localhost',
-    'accountId' => '9245359-sb1'
-];
